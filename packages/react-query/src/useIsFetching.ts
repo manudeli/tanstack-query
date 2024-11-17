8b34c5a3500331a05b39042064a8a77b30bac035
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-11-17 13:13:52 +0900

    fix(react-query): useIsFetching's getQueryCache more clearly

--- packages/react-query/src/useIsFetching.ts
+++ packages/react-query/src/useIsFetching.ts
@@ -1,7 +1,6 @@
 'use client'
 import * as React from 'react'
 import { notifyManager } from '@tanstack/query-core'
-
 import { useQueryClient } from './QueryClientProvider'
 import type { QueryClient, QueryFilters } from '@tanstack/query-core'
 
@@ -10,13 +9,14 @@ export function useIsFetching(
   queryClient?: QueryClient,
 ): number {
   const client = useQueryClient(queryClient)
-  const queryCache = client.getQueryCache()
 
   return React.useSyncExternalStore(
     React.useCallback(
       (onStoreChange) =>
-        queryCache.subscribe(notifyManager.batchCalls(onStoreChange)),
-      [queryCache],
+        client
+          .getQueryCache()
+          .subscribe(notifyManager.batchCalls(onStoreChange)),
+      [client],
     ),
     () => client.isFetching(filters),
     () => client.isFetching(filters),
